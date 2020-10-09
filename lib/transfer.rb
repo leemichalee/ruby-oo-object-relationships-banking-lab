15c0b24f9a44793e0a5653744cad06a4d203e094
leemichalee <70118534+leemichalee@users.noreply.github.com>
2020-10-09 10:07:35 -0400

    Add files via upload

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require_relative "bank_account.rb"
 class Transfer
-  # your code here
+  
+  attr_accessor :status, :amount
+  attr_reader :sender, :receiver
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    # what it does
+    #incoming ? true : false
+    #outgoing ? true : false
+    @sender.valid? && @receiver.valid? && @sender.balance > @amount
+  end
+
+  def execute_transaction
+    if valid? && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    elsif valid? == false
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+
 end
